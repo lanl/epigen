85a536a3c6f9180c086d03626af2f3684daa94cf
Anastasiia Kim <35973521+anastasiiakim@users.noreply.github.com>
2024-07-15 10:51:33 -0600

    Update create_pvdf.py

--- create_pvdf.py
+++ create_pvdf.py
@@ -1,20 +1,18 @@
-#input: hg38_chrID_200data files that are generated 
+#input: hg38_chrID_200data files that are generated through run_get200bpvalues_for_chromosome_hg38_script
+#input: genome_df38.csv contains Target variable that we used as indicies for pvdf_chr_id.pkl dataframes (any other variables or just numbers can be used, this is for tracking purposes as samples arranged in the same order as in genome_df38.csv file)
+
 import pandas as pd
 import numpy as np
 import h5py
 import sys
 
-start_p = 0
-divisor = 200
-binwidth = 200
-setbp = 500 
-#all
+
 df38 = pd.read_csv("./genome_df38.csv", delimiter=",")
 df38 = pd.DataFrame(df38)
 df38 = df38.loc[:, ['Accession', 'Target', 'Biosample term name', 'Genome']]
 for chr_id in range(int(float(sys.argv[1])), int(float(sys.argv[2]))):
     print(chr_id)
-    filename = "./results38/fin/hg38_chr" + str(chr_id) + "_200data"
+    filename = "./results38/hg38_chr" + str(chr_id) + "_200data"
     with h5py.File(filename + '.h5', 'r') as hdf:
         data_group = hdf.get('data')
         # Retrieve each component of the DataFrame
@@ -26,8 +24,8 @@ for chr_id in range(int(float(sys.argv[1])), int(float(sys.argv[2]))):
     df.index = df38['Target']
     c = pd.DataFrame(df.max(axis='rows'))
     c.columns = ['max']
-    c = c.loc[c['max'] >= 1.3, 'max'] 
+    c = c.loc[c['max'] >= 1.3, 'max'] #original data are signal p-values (-log10(regular p-value)), here we'll only extract columns where regular p-value <= 0.05
     pv_df = 10**(-df.iloc[:, list(map(int, c.index))])
     pv_df.index = range(0, len(pv_df))
-    pv_df.to_pickle("./results38/fin/pvdf_" + str(chr_id) + ".pkl")
+    pv_df.to_pickle("./results38/pvdf_" + str(chr_id) + ".pkl")
 
